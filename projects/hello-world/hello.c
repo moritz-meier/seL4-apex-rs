f968fc0313d9d111765908fd7632beb420183de6
Moritz Meier <mam14@tu-clausthal.de>
2023-01-21 03:11:23 +0100

    add hello-world

---
+++ projects/hello-world/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <camkes.h>
+
+int run(void) {
+
+  printf("Hello CAmkES World\n");
+
+  return 0;
+}
---
+++ settings.cmake
@@ -0,0 +1,31 @@
+cmake_minimum_required(VERSION 3.18.4)
+
+set(project_dir "${CMAKE_CURRENT_LIST_DIR}")
+file(GLOB project_modules "${project_dir}/projects/*")
+list(
+    APPEND
+        CMAKE_MODULE_PATH
+        "${project_dir}/kernel"
+        "${project_dir}/tools/seL4/cmake-tool/helpers/"
+        "${project_dir}/tools/seL4/elfloader-tool/"
+        "${project_modules}"
+)
+
+set(SEL4_CONFIG_DEFAULT_ADVANCED ON)
+
+include(application_settings)
+
+if(ARM_HYP)
+    set(KernelArmHypervisorSupport ON CACHE BOOL "" FORCE)
+endif()
+
+correct_platform_strings()
+
+find_package(seL4 REQUIRED)
+sel4_configure_platform_settings()
+
+if(SIMULATION)
+    ApplyCommonSimulationSettings(${KernelSel4Arch})
+endif()
+
+ApplyCommonReleaseVerificationSettings(${RELEASE} FALSE)
