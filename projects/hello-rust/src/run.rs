551b5af556ac8883c2e01c1fdce13c85831108cb
Moritz Meier <mam14@tu-clausthal.de>
2023-01-23 04:32:11 +0100

    wip

--- projects/hello-rust/src/run.rs
+++ projects/hello-rust/src/run.rs
@@ -1,25 +1,17 @@
 #![no_std]
 
-mod camkes {
-
-    #![allow(non_upper_case_globals)]
-    #![allow(non_camel_case_types)]
-    #![allow(non_snake_case)]
-    #![allow(dead_code)]
-
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
 #[cfg(not(test))] //workaround
 #[panic_handler]
 fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
     loop {}
 }
 
+use camkes_bindgen::printf;
+
 #[no_mangle]
 pub extern "C" fn run() -> isize {
     unsafe {
-        camkes::printf("Hello Camkes, Rust!\n\0".as_ptr());
+        printf("Hello Camkes, Rust!\n\0".as_ptr());
     }
 
     0
