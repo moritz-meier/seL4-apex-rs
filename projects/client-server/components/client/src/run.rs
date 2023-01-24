53d6e442b89fe8477b5146d37c8895d4bb8bc770
Moritz Meier <mam14@tu-clausthal.de>
2023-01-24 04:44:14 +0100

    wip

--- projects/client-server/components/client/src/run.rs
+++ projects/client-server/components/client/src/run.rs
@@ -9,36 +9,44 @@ fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
 #[no_mangle]
 extern "C" fn rust_eh_personality() {}
 
-use camkes_bindgen::client::{calc_add, calc_div, calc_mul, calc_sub, get_instance_name, printf};
+use camkes_bindgen::client::{
+    calc_add, calc_div, calc_mul, calc_sub, get_instance_name, id, printf,
+};
 
 #[no_mangle]
 pub extern "C" fn run() -> isize {
     unsafe {
-        printf("Starting %s ...\n\0".as_ptr(), get_instance_name());
+        let name = get_instance_name();
+
+        printf("Starting %s id: %d ...\n\0".as_ptr(), name, id);
 
         let a = 123;
         let b = 157;
         let res = calc_add(a, b);
 
-        printf("calc_add(%d, %d) = %d\n\0".as_ptr(), a, b, res);
+        printf("%s: calc_add(%d, %d) = %d\n\0".as_ptr(), name, a, b, res);
 
         let a = 133;
         let b = 17;
         let res = calc_sub(a, b);
 
-        printf("calc_sub(%d, %d) = %d\n\0".as_ptr(), a, b, res);
+        printf("%s: calc_sub(%d, %d) = %d\n\0".as_ptr(), name, a, b, res);
 
         let a = 78;
         let b = 138;
         let res = calc_mul(a, b);
 
-        printf("calc_mul(%d, %d) = %d\n\0".as_ptr(), a, b, res);
+        printf("%s: calc_mul(%d, %d) = %d\n\0".as_ptr(), name, a, b, res);
 
         let a = 123;
         let b = 38;
         let res = calc_div(a, b);
 
-        printf("calc_div(%d, %d) = %d\n\0".as_ptr(), a, b, res);
+        printf("%s: calc_div(%d, %d) = %d\n\0".as_ptr(), name, a, b, res);
+
+        loop {
+            printf("%s: ....\n\0".as_ptr(), name);
+        }
     }
 
     0
