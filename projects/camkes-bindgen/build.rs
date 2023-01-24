d7b41815e27473aa186659ba3497320f80082674
Moritz Meier <mam14@tu-clausthal.de>
2023-01-24 05:10:30 +0100

    wip

--- projects/camkes-bindgen/build.rs
+++ projects/camkes-bindgen/build.rs
@@ -15,9 +15,12 @@ fn main() -> Result<()> {
     let out_dir: PathBuf = env::var("OUT_DIR")?.into();
 
     let camkes_headers = get_camkes_headers(&build_dir)?;
+    let include_dirs: Vec<PathBuf> = get_include_dirs(&build_dir)?.collect();
+
     let mut includes = Vec::new();
+
     for (name, camkes_header) in camkes_headers {
-        generate_bindings(&build_dir, &out_dir, &name, &camkes_header)?;
+        generate_bindings(&out_dir, &name, &camkes_header, include_dirs.iter())?;
 
         includes.push(format!("pub mod {name} {{ include!(\"{name}.rs\"); }}"));
         println!(
@@ -42,8 +45,8 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn get_camkes_headers<P: AsRef<Path>>(
-    build_dir: P,
+fn get_camkes_headers(
+    build_dir: impl AsRef<Path>,
 ) -> Result<impl Iterator<Item = (String, PathBuf)>> {
     let build_dir = build_dir.as_ref().to_path_buf();
     let camkes_pathes = glob(build_dir.join("*/include/camkes.h").to_str().unwrap())?;
@@ -62,7 +65,7 @@ fn get_camkes_headers<P: AsRef<Path>>(
     }))
 }
 
-fn get_include_dirs<P: AsRef<Path>>(build_dir: P) -> Result<impl Iterator<Item = PathBuf>> {
+fn get_include_dirs(build_dir: impl AsRef<Path>) -> Result<impl Iterator<Item = PathBuf>> {
     let build_dir = build_dir.as_ref().to_path_buf();
 
     let build_file_path = build_dir.join("build.ninja");
@@ -81,27 +84,28 @@ fn get_include_dirs<P: AsRef<Path>>(build_dir: P) -> Result<impl Iterator<Item =
         .filter_map(|line| line.ok())
         .filter_map(get_includes_line)
         .flat_map(get_include_dirs)
-        //.flatten()
         .unique()
         .map(move |dir| build_dir.join(dir)))
 }
 
-fn generate_bindings<P: AsRef<Path>>(
-    build_dir: P,
-    out_dir: P,
-    name: &str,
-    camkes_header: P,
+fn generate_bindings(
+    out_dir: impl AsRef<Path>,
+    name: impl AsRef<str>,
+    camkes_header: impl AsRef<Path>,
+    include_dirs: impl Iterator<Item = impl AsRef<Path>>,
 ) -> Result<()> {
-    let include_dirs = get_include_dirs(&build_dir)?;
-
     let bindings = Builder::default()
         .header(camkes_header.as_ref().to_str().unwrap())
-        .clang_args(include_dirs.map(|dir| format!("-I{}", dir.to_str().unwrap())))
+        .clang_args(
+            include_dirs
+                .into_iter()
+                .map(|dir| format!("-I{}", dir.as_ref().to_str().unwrap())),
+        )
         .use_core()
         .layout_tests(false)
         .generate()?;
 
-    bindings.write_to_file(out_dir.as_ref().join(format!("{name}.rs")))?;
+    bindings.write_to_file(out_dir.as_ref().join(format!("{}.rs", name.as_ref())))?;
 
     Ok(())
 }
